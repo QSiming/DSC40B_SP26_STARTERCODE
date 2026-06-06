c2dc606a383b6092b9eabb885b3d042f5f892bcd
Siming Qi <s6qi@ucsd.edu>
2026-06-05 18:04:54 -0700

    Add files via upload

---
+++ slc.py
@@ -0,0 +1,34 @@
+import dsc40graph
+
+def slc(graph, d, k):
+    '''
+    Performs single linkage clustering using Kruskal's algorithm.
+    '''
+    # TODO: Implement the slc function
+
+    nodes = list(graph.nodes)
+    forest = DisjointSetForest(nodes)
+    num_clusters = len(nodes)
+
+    # Kruskal: consider edges from smallest distance to largest distance.
+    edges = sorted(list(graph.edges), key=d)
+
+    for u, v in edges:
+        # Stop as soon as we have exactly k clusters.
+        if num_clusters == k:
+            break
+
+        # Only merge if this edge connects two different clusters.
+        if not forest.in_same_set(u, v):
+            forest.union(u, v)
+            num_clusters -= 1
+
+    # Group nodes by their current representative.
+    clusters = {}
+    for node in nodes:
+        rep = forest.find_set(node)
+        if rep not in clusters:
+            clusters[rep] = set()
+        clusters[rep].add(node)
+
+    return frozenset(frozenset(cluster) for cluster in clusters.values())
